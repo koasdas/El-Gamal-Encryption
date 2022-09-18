ddabdd7f4c8b2ffe7d04e322aa12fa195a50c717
koasdas <103441487+koasdas@users.noreply.github.com>
2022-09-19 00:27:33 +0800

    Update elgamal.py

--- elgamal.py
+++ elgamal.py
@@ -8,3 +8,12 @@ def keygen():
     sk = random.randint(1,q)
     pk = pow(g, sk, p)
     return pk,sk
+
+
+def encrypt(pk,m):
+    q = (p-1)/2
+    r = random.randint(1,q)
+        
+    c1 = pow(g,r,p)
+    c2 = pow(pow(pk,r)*m, 1, p)
+    return [c1,c2]
